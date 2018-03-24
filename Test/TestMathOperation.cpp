6741508be7b50d27d8d2a9579ddbbd9f29aeb165
ngocanh198 <ngocanh198@gmail.com>
2018-03-24 23:24:26 +0700

    Correct failed test case

--- Test/TestMathOperation.cpp
+++ Test/TestMathOperation.cpp
@@ -32,7 +32,7 @@ void TestMathOperation::testAddition4()
 void TestMathOperation::testAddition5()
 {
     MathOperation mathOperation;
-    QCOMPARE(mathOperation.addTwoNumber(-1,-1),-1); //QCOMPARE( actual, expected)
+    QCOMPARE(mathOperation.addTwoNumber(-1,-1),-2); //QCOMPARE( actual, expected)
 }
 
 void TestMathOperation::testAddition6()
@@ -44,5 +44,5 @@ void TestMathOperation::testAddition6()
 void TestMathOperation::testAddition7()
 {
     MathOperation mathOperation;
-	QCOMPARE(mathOperation.addTwoNumber(-91233619,1261234),1659129); //QCOMPARE( actual, expected)
+	QCOMPARE(mathOperation.addTwoNumber(-91233619,1261234),-89972385); //QCOMPARE( actual, expected)
 }

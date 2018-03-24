7af2ef64ac1bad07bb9a511197072ad6c1c67e9c
ngocanh198 <ngocanh198@gmail.com>
2018-03-24 23:15:26 +0700

    Multiple test case

--- Test/TestMathOperation.cpp
+++ Test/TestMathOperation.cpp
@@ -5,14 +5,44 @@ TestMathOperation::TestMathOperation()
 
 }
 
-void TestMathOperation::testAddition()
+void TestMathOperation::testAddition1()
 {
     MathOperation mathOperation;
-    QCOMPARE(mathOperation.addTwoNumber(2,3),5); //QCOMPARE( actual, expected)
-    QCOMPARE(mathOperation.addTwoNumber(0,0),0); //QCOMPARE( actual, expected)
-    QCOMPARE(mathOperation.addTwoNumber(-1,1),0); //QCOMPARE( actual, expected)
-    QCOMPARE(mathOperation.addTwoNumber(2,-3),-1); //QCOMPARE( actual, expected)
-    QCOMPARE(mathOperation.addTwoNumber(-1,-1),-1); //QCOMPARE( actual, expected)
-    QCOMPARE(mathOperation.addTwoNumber(-2,-3),-5); //QCOMPARE( actual, expected)
+    QCOMPARE(mathOperation.addTwoNumber(2,3),5); //QCOMPARE( actual, expected) 
 }
 
+void TestMathOperation::testAddition2()
+{
+    MathOperation mathOperation;
+	QCOMPARE(mathOperation.addTwoNumber(0,0),0); //QCOMPARE( actual, expected)
+}
+
+void TestMathOperation::testAddition3()
+{
+    MathOperation mathOperation;
+    QCOMPARE(mathOperation.addTwoNumber(-1,1),0); //QCOMPARE( actual, expected)
+}
+
+void TestMathOperation::testAddition4()
+{
+    MathOperation mathOperation;
+    QCOMPARE(mathOperation.addTwoNumber(2,-3),-1); //QCOMPARE( actual, expected)
+}
+
+void TestMathOperation::testAddition5()
+{
+    MathOperation mathOperation;
+    QCOMPARE(mathOperation.addTwoNumber(-1,-1),-1); //QCOMPARE( actual, expected)
+}
+
+void TestMathOperation::testAddition6()
+{
+    MathOperation mathOperation;
+	QCOMPARE(mathOperation.addTwoNumber(-2,-3),-5); //QCOMPARE( actual, expected)
+}
+
+void TestMathOperation::testAddition7()
+{
+    MathOperation mathOperation;
+	QCOMPARE(mathOperation.addTwoNumber(-91231643619,126451761234),1659121745129); //QCOMPARE( actual, expected)
+}

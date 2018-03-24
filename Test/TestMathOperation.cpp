b5e39581e1017c104ae74729b40ed39f1be2b9c6
pancuro <ngocanh198@gmail.com>
2018-03-24 22:55:23 +0700

    Update TestMathOperation.cpp
    Test failed test case

--- Test/TestMathOperation.cpp
+++ Test/TestMathOperation.cpp
@@ -12,5 +12,7 @@ void TestMathOperation::testAddition()
     QCOMPARE(mathOperation.addTwoNumber(0,0),0); //QCOMPARE( actual, expected)
     QCOMPARE(mathOperation.addTwoNumber(-1,1),0); //QCOMPARE( actual, expected)
     QCOMPARE(mathOperation.addTwoNumber(2,-3),-1); //QCOMPARE( actual, expected)
+    QCOMPARE(mathOperation.addTwoNumber(-1,-1),-1); //QCOMPARE( actual, expected)
+    QCOMPARE(mathOperation.addTwoNumber(-2,-3),-5); //QCOMPARE( actual, expected)
 }
 

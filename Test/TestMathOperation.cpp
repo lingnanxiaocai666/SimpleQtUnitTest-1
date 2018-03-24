2eee1fa6977b8aab19b1c5c76c9e2819fe02ebef
pancuro <ngocanh198@gmail.com>
2018-03-24 22:51:04 +0700

    Update TestMathOperation.cpp

--- Test/TestMathOperation.cpp
+++ Test/TestMathOperation.cpp
@@ -9,5 +9,8 @@ void TestMathOperation::testAddition()
 {
     MathOperation mathOperation;
     QCOMPARE(mathOperation.addTwoNumber(2,3),5); //QCOMPARE( actual, expected)
+    QCOMPARE(mathOperation.addTwoNumber(0,0),0); //QCOMPARE( actual, expected)
+    QCOMPARE(mathOperation.addTwoNumber(-1,1),0); //QCOMPARE( actual, expected)
+    QCOMPARE(mathOperation.addTwoNumber(2,-3),-1); //QCOMPARE( actual, expected)
 }
 
